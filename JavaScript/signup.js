5756c93bf5629bc95a91872a8386844f893f840d
Wang-Yi-Chun <68949265+Wang-Yi-Chun@users.noreply.github.com>
2020-08-03 05:11:57 +0800

    Create

---
+++ JavaScript/signup.js
@@ -0,0 +1,24 @@
+let sendButton = document.querySelector('button');
+
+function send(){
+	let lastname = document.querySelector('#lastnameValue').value;
+	let firstname = document.querySelector('#firstnameValue').value;
+	let email = document.querySelector('#emailValue').value;
+	let password1 = document.querySelector('#password1Value').value;
+	$.ajax({
+		url: "https://script.google.com/macros/s/AKfycbxSD9XCcxkjGCDVFwJ3Hj0ervVlrLOg2ibDpTIO8p6cEQIOsZCl/exec",
+		data: {
+			"lastname": lastname,
+			"firstname": firstname,
+			"email": email,
+			"password1": password1
+		},
+		success: function(response){
+			if(response == "成功"){
+				alert("註冊成功!");
+			}
+		},
+	});
+};
+
+sendButton.addEventListener('click', send);
