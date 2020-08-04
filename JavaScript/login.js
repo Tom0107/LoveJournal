75279892e4549dbb09f44d693023096824365333
Wang-Yi-Chun <68949265+Wang-Yi-Chun@users.noreply.github.com>
2020-08-05 00:39:37 +0800

    Update login.js

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -3,10 +3,10 @@ function login(){
 	account=document.getElementById("exampleDropdownFormEmail1");
 	password=document.getElementById("exampleDropdownFormPassword1");
 	if(account.value===""){
-		alert("請輸入帳號，謝謝!!")
+		alert("請輸入帳號，謝謝!!!")
 	}
 	else if(password.value===""){
-		alert("請輸入密碼，謝謝!!")
+		alert("請輸入密碼，謝謝!!*")
 	}
 	else if(account.value==="0000@gmail.com" && password.value==="0000"){
 		form.action="HTML/journal.html";
