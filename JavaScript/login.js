fa421be51b5a092430a198d6b4477cb1e2b8fd9c
Wang-Yi-Chun <68949265+Wang-Yi-Chun@users.noreply.github.com>
2020-08-05 00:35:48 +0800

    Create

---
+++ JavaScript/login.js
@@ -0,0 +1,21 @@
+function login(){
+	let account,password;
+	account=document.getElementById("exampleDropdownFormEmail1");
+	password=document.getElementById("exampleDropdownFormPassword1");
+	if(account.value===""){
+		alert("請輸入帳號，謝謝!!")
+	}
+	else if(password.value===""){
+		alert("請輸入密碼，謝謝!!")
+	}
+	else if(account.value==="0000@gmail.com" && password.value==="0000"){
+		form.action="HTML/journal.html";
+		form.submit();
+	}
+	else{
+		alert("請輸入正確的帳號與密碼，謝謝!!")
+	}
+}
+function forget(){
+	alert("自己想辦法啊~")
+}
